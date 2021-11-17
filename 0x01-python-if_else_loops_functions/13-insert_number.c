78bba93231b46796d257e4cae3f34806560db081
dk-Gk <idkin19@gmail.com>
2021-11-17 23:33:58 +0000

    python script

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -8,46 +8,41 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new;
-	listint_t *current;
-	listint_t *temp;
-
-	current = *head;
-	temp = *head;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = number;
-	if (*head == NULL)
-	{
-		*head = new;
-		new->next = NULL;
-	}
-	else
-	{
-		while (current->next != NULL)
-		{
-			if (current->n > new->n)
-			{
-				if (current == *head)
-				{
-					new->next = current;
-					*head = new;
-				}
-				while (temp->next != current)
-				{
-					temp = temp->next;
-				}
-				temp->next = new;
-				new->next = current;
-				return (new);
-			}
-			current = current->next;
-		}
-		current->next = new;
-		new->next = NULL;
-	}
-	return (new);
+listint_t *new;
+listint_t *current;
+listint_t *temp;
+current = *head;
+temp = *head;
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+return (NULL);
+new->n = number;
+if (*head == NULL)
+{
+*head = new;
+new->next = NULL;
+}
+else
+{
+while (current->next != NULL)
+{
+if (current->n > new->n)
+{
+if (current == *head)
+{
+new->next = current;
+*head = new;
+}
+while (temp->next != current)
+temp = temp->next;
+temp->next = new;
+new->next = current;
+return (new);
+}
+current = current->next;
+}
+current->next = new;
+new->next = NULL;
+}
+return (new);
 }

9239d143df4b4dff347fc1f81523a590954c14f2
dk-Gk <idkin19@gmail.com>
2021-11-18 22:18:46 +0000

    python import module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,12 +3,11 @@ if __name__ == "__main__":
     import sys
     a = len(sys.argv)
     if (a == 2):
-        print("{} arument:".format(1))
-        print("{}: {}".format(1), sys.argv[1])
+        print("1 arument:")
     else:
         if (a == 1):
-            print("{} aruments.".format(a - 1))
+            print("0 aruments.")
         else:
             print("{} aruments:".format(a - 1))
-            for i in range(len(a - 1):
-                print("{}: {}".format(i + 1), sys.argv[i + 1])
+        for i in range(len(a - 1):
+            print("{}: {}".format(i + 1), sys.argv[i + 1])

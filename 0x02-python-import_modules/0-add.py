64ee864619a6cf79982681626182292a0ba46e39
dk-Gk <idkin19@gmail.com>
2021-11-18 21:53:10 +0000

    python import module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0 as add
+from add_0.py import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{} + {} = {}".format(a,b,add.add(a, b)))
+    print("{} + {} = {}".format(a,b,add(a, b)))

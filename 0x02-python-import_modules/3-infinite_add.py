299b966fe2caf46013ebb347f0243b60deecdfd9
dk-Gk <idkin19@gmail.com>
2021-11-18 22:51:55 +0000

    python import module

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
-    a = sys.argv
+    import sys
+    a = len(sys.argv) - 1
     sum = 0
-    if (len(a) == 2):
-        print("{}".format(int(a[1])))
-    else:
-        if (len(a) == 1):
-            print("{}".format(len(a) - 1))
-        else:
-            for i in range(len(a) - 1):
-                sum = sum + int(a[i + 1])
-                print("{}".format(sum))
+    for i in range(a):
+        sum = sum + int(a[i + 1])
+    print("{}".format(sum))

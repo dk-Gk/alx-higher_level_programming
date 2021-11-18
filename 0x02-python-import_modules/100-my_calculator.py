cf2e192b893e02d09db8a6db7780283e4a4839a1
dk-Gk <idkin19@gmail.com>
2021-11-18 23:22:51 +0000

    python import module

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-import sys
+
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
     a = len(sys.argv) - 1
     oper = {"+": add, "-": sub, "*": mul, "/": div}
-    if (a != 3):
+    if a != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     if sys.argv[2] not in  list(oper.keys()):

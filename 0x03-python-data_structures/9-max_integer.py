f09ea2bc70480abfef9773872df9e173f5c90938
dk-Gk <idkin19@gmail.com>
2021-11-21 22:31:15 +0000

    python data structure

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    maxx = my_list[0]
     if len(my_list) == 0:
         return None
+    maxx = my_list[0]
     for i in range(len(my_list)):
         if my_list[i] > maxx:
             maxx = my_list[i]

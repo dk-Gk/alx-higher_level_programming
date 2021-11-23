5bca234d317e8ba533189cbcd609174fd6f64797
dk-Gk <idkin19@gmail.com>
2021-11-23 14:18:27 +0000

    a function in C that checks if a singly linked list is a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,29 +1,44 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * palindrom -  recursive palind or not
- * @head: head list
- * Return: 0 if it is not a palindrome
- * 1 if it is a palindrome
+ * is_palindrome - check if a linked list is palindrome or not
+ * @head: pointer to head of list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	if (head == NULL || *head == NULL)
-		return (1);
-	return (aux_palind(head, *head));
+
+listint_t *temp2;
+temp2 = *head;
+int arr[9999];
+int i = 0, count = 0, j;
+if ((!*head) || (!head))
+{
+return (1);
+}
+if (!temp2->next)
+{
+return (1);
 }
 
-/**
- * aux_palind - funct to know if is palindrome
- * @head: head list
- * @end: end list
- */
-int aux_palind(listint_t **head, listint_t *end)
+while (temp2->next != NULL)
 {
-	if (end == NULL)
-		return (1);
-	if (aux_palind(head, end->next) && (*head)->n == end->n)
-	{
-		*head = (*head)->next;
-		return (1);
-	}
-	return (0);
+arr[i] = temp2->n;
+temp2 = temp2->next;
+i++;
+count++;
+}
+arr[i] = temp2->n;
+count++;
+j = count - 1;
+for (i = 0; i < count; i++)
+{
+if (arr[i] != arr[j])
+{
+return (0);
+}
+j--;
+}
+return (1);
 }

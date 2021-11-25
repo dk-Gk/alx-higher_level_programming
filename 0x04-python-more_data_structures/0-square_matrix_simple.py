e15aeb7e5584372728510913a9d65ebe77619655
dk-Gk <idkin19@gmail.com>
2021-11-25 01:51:20 +0000

    python data structure

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 def square_matrix_simple(matrix=[]):
     new = []
     for i in range(len(matrix)):
-        sq = list(map(lambda x: x*x , matrix[i]))
+        sq = list(map(lambda x: x*x, matrix[i]))
         new.append(sq)
     return new

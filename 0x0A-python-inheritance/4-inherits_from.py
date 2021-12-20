30d047f3f67aaf7f2a233593856854351e5b280e
dk-Gk <idkin19@gmail.com>
2021-12-20 23:34:02 +0000

    python-inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """inherits_from"""
+
+
 def inherits_from(obj, a_class):
     """A function that returns True if the object is
     an instance of a class that inherited (directly or indirectly)

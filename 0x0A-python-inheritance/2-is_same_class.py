584b2beb7cd86bf4f6998b8696b04c23dac46b5e
dk-Gk <idkin19@gmail.com>
2021-12-20 23:31:06 +0000

    python-inheritance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """is_same_class"""
 
+
 def is_same_class(obj, a_class):
     """return true if obj is the exact class a_class, 
-    otherwise false"""
+    otherwise false
+    """
     return (type(obj) == a_class)
 

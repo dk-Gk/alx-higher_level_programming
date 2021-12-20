2b961baf979ec3360accfe758f8c020d88730a5d
dk-Gk <idkin19@gmail.com>
2021-12-20 23:03:48 +0000

    advanced python-inheritance

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class MyInt"""
+class MyInt(int):
+    """A class that inherits from int"""
+
+    def __eq__(self, value):
+        """Not equating the operator"""
+        return self.real != value
+    
+    def __ne__(self, value):
+        """Equating the operator"""
+        return self.real == value

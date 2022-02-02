cd5c0394763787f3bde8606d179040164746cec3
dk-Gk <idkin19@gmail.com>
2022-02-02 01:13:15 +0000

    SQL introduction

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,2 @@
 -- Script that creates a table called first_table in the current database
-CREATE TABLE IF NOT EXISTS first_table
-(
-	id INT,
-	name VARCHAR(25
-);	
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));

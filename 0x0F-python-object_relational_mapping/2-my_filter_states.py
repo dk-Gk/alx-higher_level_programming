25821ce10088d059a5d62fb2ad2cce665a40aaa4
dk-Gk <idkin19@gmail.com>
2022-03-07 01:03:12 +0000

    python ORM

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""script that takes in an argument and displays all values in the states table"""
+
+import sys
+import MySQLdb
+
 
 if __name__ == "__main__":
-    import sys
-    import MySQLdb
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
                            charset="utf8")
     cur = conn.cursor()

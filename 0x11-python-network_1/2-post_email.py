dcc92fcdb4423c749e32dc404e0029840f1d4d32
dk-Gk <idkin19@gmail.com>
2022-03-19 03:52:05 +0000

    pyNet

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,8 +2,9 @@
 """POST an email"""
 
 if __name__ == "__main__":
+    import sys
     import urllib.request
     import urllib.parse
-    d = urllib.parse.urlencode({'email': argv[2]}).encode('ascii')
-    with urllib.request.urlopen(url=argv[1], data=d) as response:
+    d = urllib.parse.urlencode({'email': sys.argv[2]}).encode('ascii')
+    with urllib.request.urlopen(url=sys.argv[1], data=d) as response:
         print(response.read().decode('UTF-8'))

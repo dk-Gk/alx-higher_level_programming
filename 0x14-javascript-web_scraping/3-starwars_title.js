d361e23f5f75ac3a0066df1ba225214f9e31e552
dk-Gk <idkin19@gmail.com>
2022-04-12 23:35:05 +0000

    Javascript

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const rs = require('request');
-rs(`http://swapi.co/api/films/${process.argv[2]}`, function (error, response, body) {
+rs(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, function (error, response, body) {
   error && console.log(error);
   console.log(JSON.parse(body).title);
 });

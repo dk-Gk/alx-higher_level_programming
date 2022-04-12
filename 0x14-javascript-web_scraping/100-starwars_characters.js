54680f2487d4fd4e76b31b72c5eb6282795002c7
dk-Gk <idkin19@gmail.com>
2022-04-12 23:45:30 +0000

    Javascript

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const rs = require('request');
-const Star_war = `http://swapi.co/api/films/${process.argv[2]}`;
-rs(Star_war, function (error, response, body) {
+const STAR_WAR = `http://swapi.co/api/films/${process.argv[2]}`;
+rs(STAR_WAR, function (error, response, body) {
   if (error) {
     throw new Error(error);
   }
-  for (const url of JSON.parse(body).characters) {
-    rs(url, (error, response, body) =>
+  for (const STAR_WAR of JSON.parse(body).characters) {
+    rs(STAR_WAR, (error, response, body) =>
       !error && console.log(JSON.parse(body).name));
   }
 });
